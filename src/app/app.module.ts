0718d12d650adaf67c67babf237d721858b30147
hscordonez <zh311505@gmail.com>
2023-10-04 22:50:27 -0500

    feat: inicializar app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NotFoundComponent } from './errors/404/not-found.component';
+import { PortafolioComponent } from './modules/portafolio/portafolio.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NotFoundComponent,
+    PortafolioComponent
   ],
   imports: [
     BrowserModule

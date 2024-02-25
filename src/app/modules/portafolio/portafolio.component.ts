5c36294b503a0e60b4471822d6c60c1afa2c3064
HECTOR STEVEEN ORDOÑEZ CHAMBA <hsc.ordonez@yavirac.edu.ec>
2024-02-24 22:51:28 -0500

    fix: lenguaje por defecto

--- src/app/modules/portafolio/portafolio.component.ts
+++ src/app/modules/portafolio/portafolio.component.ts
@@ -14,100 +14,12 @@ import { Router } from '@angular/router';
 })
 export class PortafolioComponent implements OnInit {
 
-
-  isMenuOpen = false;
-  showMenuDown = true;
-  isMenuOpenEmail = false;
   isModalOpen = false;
-  isModalOpenEmail = false;
-
-  // showMenuDownEmail = true;
-  // mostrarPrimaria = false;
-  // mostrarSecundaria = false;
-  // formGroup!: FormGroup;
 
   constructor(
-    public _translate: TranslateService,
     private el: ElementRef,
-    private renderer: Renderer2,
-    @Inject(DOCUMENT) private document: Document,
-    private fb:FormBuilder,
     private themeService: ThemeService,
-    private _router: Router,
-  ) {
-    // this.initForm();
-  }
-
-
-    // //informations
-    // openDialogInformationBranch(): void {
-    //   const dialogRef = this.openInformationsBranchDialog();
-  
-    //   dialogRef.afterClosed().subscribe(() => {
-        
-    //       this.navigateToBranchList()
-        
-    //   });
-    // }
-  
-    // private navigateToBranchList(): void {
-    //   this._router.navigate(['/system/sucursales']);
-    // }
-  
-    // public openInformationsBranchDialog(): MatDialogRef<ModalProfileComponent> {
-    //   return this._dialog.open(ModalProfileComponent, {
-    //     height: '20px',
-    //     width: '50px',
-    //   });
-    // }
-  
-
-  // initForm() {
-  //   this.formGroup = this.fb.group({
-  //     from_name:[
-  //       '',
-  //       {
-  //         validators: [
-  //           Validators.required,
-  //           Validators.minLength(2)
-  //         ]
-  //       }
-  //     ],
-  //     to_name:'Admin',
-  //     from_email:['', [Validators.required, Validators.email]],
-  //     subject:[
-  //       '',
-  //       {
-  //         validators: [
-  //           Validators.required,
-  //           Validators.minLength(2)
-  //         ]
-  //       }
-  //     ],
-  //     message:[
-  //       '',
-  //       {
-  //         validators: [
-  //           Validators.required,
-  //           Validators.minLength(2)
-  //         ]
-  //       }
-  //     ],
-  //   })
-  // }
-
-  // async send() {
-  //   emailjs.init('qIVLgZYRhPQybiqfI')
-  //   let response = await emailjs.send('service_rtyq5ds','template_b6reeee',{
-  //     from_name:this.formGroup.value.from_name,
-  //     to_name:this.formGroup.value.to_name,
-  //     from_email:this.formGroup.value.from_email,
-  //     subject:this.formGroup.value.subject,
-  //     message:this.formGroup.value.message,
-  //   });
-  //   alert('Message has been sent');
-  //   this.formGroup.reset();
-  // }
+  ) {}
 
   ngOnInit(): void {
     this.themeService.theme$.subscribe(theme => {
@@ -119,7 +31,6 @@ export class PortafolioComponent implements OnInit {
     this.el.nativeElement.dataset.theme = theme;
   }
 
-
   openModal() {
     this.isModalOpen = true;
   }
@@ -128,61 +39,4 @@ export class PortafolioComponent implements OnInit {
     this.isModalOpen = false;
   }
 
-  closeModalEmail() {
-    this.isModalOpenEmail = false;
-  }
-
-  // titles = [
-  //   "'Skill' | translate }}"
-  // ];
-
-  toggleMenu() {
-    this.isMenuOpen = !this.isMenuOpen;
-  }
-
-
-  abrirModal(): void {
-    this.isMenuOpen = true;
-  }
-
-  cerrarModal(): void {
-    this.isMenuOpen = false;
-  }
-
-  // mostrarImagenSecundaria(): void {
-  //   this.mostrarPrimaria = false;
-  //   this.mostrarSecundaria = true;
-  // }
-  
-  // ocultarImagenSecundaria(): void {
-  //   this.mostrarPrimaria = true;
-  //   this.mostrarSecundaria = false;
-  // }
-  
-  @HostListener('window:resize', ['$event'])
-  onResize(event: any) {
-    const windowHeight = window.innerHeight;
-    const buttonBottom = document.getElementById('toggleButton')?.getBoundingClientRect().bottom || 0;
-    this.showMenuDown = buttonBottom < windowHeight / 2;
-
-  }
-
-  // toggleMenuEmail() {
-  //   this.isMenuOpenEmail = !this.isMenuOpenEmail;
-  // }
-
-  // cerrarModalEmail(): void {
-  //   this.isMenuOpenEmail = false;
-  // }
-
-  // @HostListener('window:resize', ['$event'])
-  // onResizeEmail(event: any) {
-  //   const windowHeight = window.innerHeight;
-  //   const buttonBottom = document.getElementById('toggleButtonEmail')?.getBoundingClientRect().bottom || 0;
-  //   this.showMenuDown = buttonBottom < windowHeight / 2;
-
-  // }
-
-
- 
 }
